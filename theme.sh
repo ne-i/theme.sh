1e739672ffdf3e28970798f6bf6c853f24d6e2d7
ne-i <ebner.niklas@gmail.com>
2021-04-25 21:34:51 +0200

    Modiefied one-dark theme, changed to a lighter foreground color.

--- theme.sh
+++ theme.sh
@@ -3241,6 +3241,27 @@ background: #1e2127
 foreground: #5c6370
 cursorColor: #5c6370
 
+one-dark2
+0: #000000
+1: #e06c75
+2: #98c379
+3: #d19a66
+4: #61afef
+5: #c678dd
+6: #56b6c2
+7: #abb2bf
+8: #5c6370
+9: #e06c75
+10: #98c379
+11: #d19a66
+12: #61afef
+13: #c678dd
+14: #56b6c2
+15: #fffefe
+background: #1e2127
+foreground: #abb2bf
+cursorColor: #5c6370
+
 one-half-black
 0: #282c34
 1: #e06c75
